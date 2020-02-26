f315aa3f533b14082606689504c1d5d9f83a8401
Loic Payn <loic.payn@worldprogramming.com>
2020-02-26 15:46:03 +0000

    Week 4 upto Q11 alomost finished

--- EXERCISES/WEEK 4 SUMMARISING DATA  - EXERCISES.sql
+++ EXERCISES/WEEK 4 SUMMARISING DATA  - EXERCISES.sql
@@ -33,13 +33,47 @@ WHERE ordid <= '605'
 GROUP BY ordid;
 
 --6
+SELECT p.prodid, p.descrip, SUM(i.qty) AS 'Quantity Sold'
+FROM product p
+   INNER JOIN item i ON p.prodid = i.prodid 
+GROUP BY p.prodid, p.descrip;
 
 --7
+SELECT p.prodid, p.descrip, SUM(i.qty) AS 'Quantity Sold'
+FROM product p
+   INNER JOIN item i ON p.prodid = i.prodid 
+   INNER JOIN ord o ON i.ordid = o.ordid
+WHERE orderdate >= '2005-02-16'
+GROUP BY p.prodid, p.descrip;
+
 
 --8
+SELECT c.name, COUNT(i.itemid) AS 'No of items ordered', printf("£%.2f",SUM(qty*actualprice)) AS 'Order Value'
+FROM customer c
+    INNER JOIN ord o ON c.custid = o.custid
+    INNER JOIN item i ON o.ordid = i.ordid
+WHERE orderdate >= '2005-02-02'
+GROUP BY c.name;
 
 --9
+SELECT c.name, COUNT(i.itemid) AS 'No of items ordered', printf("£%.2f",SUM(qty*actualprice)) AS 'Order Value'
+FROM customer c
+    INNER JOIN ord o ON c.custid = o.custid
+    INNER JOIN item i ON o.ordid = i.ordid
+WHERE orderdate >= '2005-02-02'
+GROUP BY c.name
+HAVING COUNT(i.itemid) >= 5;
 
 --10
+SELECT e.ename, d.dname, COUNT(i.itemid) AS 'No of items ordered', printf("£%.2f",SUM(i.qty*i.actualprice)) AS 'Order Value'
+FROM dept d
+    INNER JOIN emp e ON d.deptno = e.deptno
+    INNER JOIN customer c ON e.empno = c.repid
+    INNER JOIN ord o ON c.custid = o.custid
+    INNER JOIN item i ON o.ordid = i.ordid
+WHERE orderdate >= '2005-02-02'
+GROUP BY e.ename, d.dname
+HAVING COUNT(o.ordid) >= 3
+AND SUM(i.qty * i.actualprice) >= 1000;
 
 --11
